d337592cd915a0406cfe3d086fd1cc94d58878cf
rightfold <rightfold@gmail.com>
2016-11-18 20:55:20 +0100

    Get rid of global inspector and vertex selection

--- src/NN/Vertex/UI.purs
+++ src/NN/Vertex/UI.purs
@@ -5,7 +5,6 @@ module NN.Vertex.UI
 , ui
 ) where
 
-import Control.Monad.Aff.Bus (BusRW)
 import Control.Monad.Aff.Bus as Bus
 import Control.Monad.State.Class as State
 import Data.List as List
@@ -21,25 +20,17 @@ import NN.Vertex.DSL (getVertex, vertexBus, VertexDSL)
 import NN.Vertex.Note (Note(..))
 import NN.Vertex.Style (styleClass)
 
-type State =
-    { vertex :: Maybe Vertex
-    , selectedVertexID :: Maybe VertexID
-    }
+type State = Maybe Vertex
 
 data Query a
     = Initialize a
     | UpdateVertex Vertex a
     | UpdateNote String a
-    | ToggleSelfSelection a
-    | UpdateSelection (Maybe VertexID) a
 
 instance functorQuery :: Functor Query where
     map f (Initialize next) = Initialize (f next)
     map f (UpdateVertex vertex next) = UpdateVertex vertex (f next)
     map f (UpdateNote text next) = UpdateNote text (f next)
-    map f (ToggleSelfSelection next) = ToggleSelfSelection (f next)
-    map f (UpdateSelection newSelectedVertexID next) =
-        UpdateSelection newSelectedVertexID (f next)
 
 type Output = Void
 
@@ -53,12 +44,12 @@ mLiftAff = liftF <<< left
 mLiftVertexDSL :: ∀ eff. VertexDSL ~> Monad eff
 mLiftVertexDSL = liftF <<< right
 
-ui :: ∀ eff. VertexID -> Set VertexID -> BusRW (Maybe VertexID) -> Component HTML Query Output (Monad eff)
-ui vertexID parentIDs selectedVertexBus =
+ui :: ∀ eff. VertexID -> Set VertexID -> Component HTML Query Output (Monad eff)
+ui vertexID parentIDs =
     lifecycleParentComponent {initialState, render, eval, initializer, finalizer}
     where
     initialState :: State
-    initialState = {vertex: Nothing, selectedVertexID: Nothing}
+    initialState = Nothing
 
     render :: State -> ParentHTML Query Query Slot (Monad eff)
     render state
@@ -69,12 +60,10 @@ ui vertexID parentIDs selectedVertexBus =
     renderCycleIndicator = H.text "(cycle)"
 
     renderVertex :: State -> ParentHTML Query Query Slot (Monad eff)
-    renderVertex {vertex: Nothing} = H.text "(loading)"
-    renderVertex {vertex: Just {note, children, style}, selectedVertexID} =
-        H.article [P.classes ([styleClass style] <> if Just vertexID == selectedVertexID then [ClassName "-selected"] else [])]
-            [ H.section [ P.class_ (ClassName "-note")
-                        , E.onClick (E.input_ ToggleSelfSelection)
-                        ] $
+    renderVertex Nothing = H.text "(loading)"
+    renderVertex (Just {note, children, style}) =
+        H.article [P.class_ (styleClass style)]
+            [ H.section [P.class_ (ClassName "-note")] $
                 renderNote note
             , H.section [P.class_ (ClassName "-children")]
                 [ H.ul [] $
@@ -89,41 +78,32 @@ ui vertexID parentIDs selectedVertexBus =
     renderNote Empty = []
     renderNote (Append a b) = renderNote a <> renderNote b
     renderNote (Text text) =
-        [ H.textarea [ E.onFocus (E.input_ ToggleSelfSelection)
-                     , E.onValueChange (Just <<< action <<< UpdateNote)
+        [ H.textarea [ E.onValueChange (Just <<< action <<< UpdateNote)
                      , P.value text
                      ]
         ]
 
     renderChild :: Slot -> VertexID -> Array (ParentHTML Query Query Slot (Monad eff))
     renderChild slot childID =
-        let childComponent = defer \_ -> ui childID (Set.insert vertexID parentIDs) selectedVertexBus
+        let childComponent = defer \_ -> ui childID (Set.insert vertexID parentIDs)
         in [H.slot slot childComponent absurd]
 
     eval :: Query ~> ParentDSL State Query Query Slot Output (Monad eff)
-    eval (Initialize next) = immediate *> subsequent *> selection $> next
+    eval (Initialize next) = immediate *> subsequent $> next
         where
-        immediate = do
-            vertex <- lift $ mLiftVertexDSL $ getVertex vertexID
-            State.modify _ {vertex = vertex}
+        immediate = State.put =<< lift (mLiftVertexDSL $ getVertex vertexID)
         subsequent = do
             bus <- lift $ mLiftVertexDSL $ vertexBus vertexID
             hoistM mLiftAff $ subscribe (busEvents bus (Just <<< (true <$ _) <<< action <<< UpdateVertex))
-        selection = hoistM mLiftAff $ subscribe (busEvents selectedVertexBus (Just <<< (true <$ _) <<< action <<< UpdateSelection))
-    eval (UpdateVertex vertex next) = next <$ State.modify _ {vertex = Just vertex}
+    eval (UpdateVertex vertex next) = next <$ State.put (Just vertex)
     eval (UpdateNote text next) = do
-        State.gets _.vertex >>= case _ of
+        State.get >>= case _ of
             Nothing -> pure unit
             Just vertex -> do
                 let newVertex = vertex {note = Text text}
                 bus <- lift $ mLiftVertexDSL $ vertexBus vertexID
                 lift $ mLiftAff $ Bus.write newVertex bus
         pure next
-    eval (ToggleSelfSelection next) = do
-        selfSelected <- State.gets \{selectedVertexID} -> selectedVertexID == Just vertexID
-        let newSelection = if selfSelected then Nothing else Just vertexID
-        next <$ lift (mLiftAff (Bus.write newSelection selectedVertexBus))
-    eval (UpdateSelection newSelectedVertexID next) = next <$ State.modify _ {selectedVertexID = newSelectedVertexID}
 
     initializer :: Maybe (Query Unit)
     initializer = Just $ action Initialize
--- src/NN/Workspace.purs
+++ src/NN/Workspace.purs
@@ -5,30 +5,16 @@ module NN.Workspace
 , ui
 ) where
 
-import Control.Monad.Aff.Bus (BusRW)
-import Control.Monad.Aff.Bus as Bus
-import Control.Monad.State.Class as State
 import Data.Set as Set
-import Halogen.Component.Bus (busEvents)
 import Halogen.HTML as H
 import NN.DSL (NNDSL)
 import NN.Prelude.Halogen
 import NN.Vertex (VertexID(..))
 import NN.Vertex.UI as Vertex.UI
 
-type State =
-    { selectedVertexBus :: BusRW (Maybe VertexID)
-    , selectedVertexID :: Maybe VertexID
-    }
+type State = Unit
 
-data Query a
-    = Initialize a
-    | UpdateSelection (Maybe VertexID) a
-
-instance functorQuery :: Functor Query where
-    map f (Initialize next) = Initialize (f next)
-    map f (UpdateSelection newSelectedVertexID next) =
-        UpdateSelection newSelectedVertexID (f next)
+newtype Query a = Query Void
 
 type Output = Void
 
@@ -36,39 +22,16 @@ type Slot = Unit
 
 type Monad eff = Free (Aff (avar :: AVAR | eff) ⊕ NNDSL)
 
-mLiftAff :: ∀ eff. Aff (avar :: AVAR | eff) ~> Monad eff
-mLiftAff = liftF <<< left
-
 ui :: ∀ eff. Component HTML Query Output (Monad eff)
-ui = lifecycleParentComponent {initialState, render, eval, initializer, finalizer}
+ui = parentComponent {initialState, render, eval}
     where
-    initialState :: Maybe State
-    initialState = Nothing
+    initialState :: State
+    initialState = unit
 
-    render :: Maybe State -> ParentHTML Query Vertex.UI.Query Slot (Monad eff)
-    render Nothing = H.div [] []
-    render (Just s) = H.div [] [renderTree s, renderInspector s]
+    render :: State -> ParentHTML Query Vertex.UI.Query Slot (Monad eff)
+    render _ =
+        let rootID = VertexID "92eacb4c-a841-4b96-a984-a077caba347c"
+        in H.slot unit (defer \_ -> Vertex.UI.ui rootID Set.empty) absurd
 
-    renderTree :: State -> ParentHTML Query Vertex.UI.Query Slot (Monad eff)
-    renderTree {selectedVertexBus} =
-        H.section []
-            let rootID = VertexID "92eacb4c-a841-4b96-a984-a077caba347c"
-            in [H.slot unit (defer \_ -> Vertex.UI.ui rootID Set.empty selectedVertexBus) absurd]
-
-    renderInspector :: State -> ParentHTML Query Vertex.UI.Query Slot (Monad eff)
-    renderInspector {selectedVertexID} = H.text (show selectedVertexID)
-
-    eval :: Query ~> ParentDSL (Maybe State) Query Vertex.UI.Query Slot Output (Monad eff)
-    eval (Initialize next) = do
-        selectedVertexBus <- lift (mLiftAff Bus.make)
-        State.put $ Just {selectedVertexBus, selectedVertexID: Nothing}
-        hoistM mLiftAff $ subscribe (busEvents selectedVertexBus (Just <<< (true <$ _) <<< action <<< UpdateSelection))
-        pure next
-    eval (UpdateSelection newSelectedVertexID next) =
-        next <$ State.modify (_ <#> _ {selectedVertexID = newSelectedVertexID})
-
-    initializer :: Maybe (Query Unit)
-    initializer = Just $ action Initialize
-
-    finalizer :: Maybe (Query Unit)
-    finalizer = Nothing
+    eval :: Query ~> ParentDSL State Query Vertex.UI.Query Slot Output (Monad eff)
+    eval (Query void) = absurd void
