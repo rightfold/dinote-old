9effd7bc7b0727e032644193c196bd4f35246d33
rightfold <rightfold@gmail.com>
2016-11-18 21:00:05 +0100

    Add grass and warning vertex styles

--- src/NN/Interpret.purs
+++ src/NN/Interpret.purs
@@ -42,7 +42,9 @@ runVertexDSL busesRef = foldFree go
         VertexID "92eacb4c-a841-4b96-a984-a077caba347c" -> Just {note: Text "root", children: VertexID "9733d16e-d506-428a-a135-c3e7d886c396" : VertexID "78c48c7e-7fad-48a8-815f-9f4c2ce43fd7" : Nil, style: Normal}
         VertexID "9733d16e-d506-428a-a135-c3e7d886c396" -> Just {note: Text "a", children: VertexID "d2e7f648-e8a7-4891-8754-4543498a8f57" : Nil, style: Dimmed}
         VertexID "78c48c7e-7fad-48a8-815f-9f4c2ce43fd7" -> Just {note: Text "b", children: VertexID "d2e7f648-e8a7-4891-8754-4543498a8f57" : VertexID "92eacb4c-a841-4b96-a984-a077caba347c" : Nil, style: Salmon}
-        VertexID "d2e7f648-e8a7-4891-8754-4543498a8f57" -> Just {note: Text "c", children: Nil, style: HotDogStand}
+        VertexID "d2e7f648-e8a7-4891-8754-4543498a8f57" -> Just {note: Text "c", children: VertexID "0fe66fb3-c195-49d1-a31b-983d8191f6a5" : VertexID "5c9151bc-df1d-4ff0-87c4-60e66642b8a9" : Nil, style: HotDogStand}
+        VertexID "0fe66fb3-c195-49d1-a31b-983d8191f6a5" -> Just {note: Text "d", children: Nil, style: Grass}
+        VertexID "5c9151bc-df1d-4ff0-87c4-60e66642b8a9" -> Just {note: Text "e", children: Nil, style: Warning}
         _ -> Nothing
     go (VertexBus vertexID a) = do
         freshBus <- Bus.make
--- src/NN/Vertex/Style.purs
+++ src/NN/Vertex/Style.purs
@@ -5,10 +5,12 @@ module NN.Vertex.Style
 
 import Halogen.HTML.Core (ClassName(..))
 
-data Style = Normal | Dimmed | Salmon | HotDogStand
+data Style = Normal | Dimmed | Grass | Warning | Salmon | HotDogStand
 
 styleClass :: Style -> ClassName
 styleClass Normal = ClassName "-style-normal"
 styleClass Dimmed = ClassName "-style-dimmed"
+styleClass Grass = ClassName "-style-grass"
+styleClass Warning = ClassName "-style-warning"
 styleClass Salmon = ClassName "-style-salmon"
 styleClass HotDogStand = ClassName "-style-hot-dog-stand"
