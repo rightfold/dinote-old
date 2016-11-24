bd078e6d8b91e789e78da6bdabce0df5b419588f
rightfold <rightfold@gmail.com>
2016-11-24 22:36:13 +0100

    Move vertex fetching code to its own module

--- src/NN/Interpret.purs
+++ src/NN/Interpret.purs
@@ -6,16 +6,12 @@ module NN.Interpret
 
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Bus (BusRW)
-import Data.Argonaut.Core as JSON
-import Data.List as List
-import Data.StrMap as StrMap
 import Network.HTTP.Affjax (AJAX)
-import Network.HTTP.Affjax as Affjax
 import NN.DSL (NNDSL)
 import NN.Prelude
-import NN.Vertex (Vertex, VertexID(..))
+import NN.Vertex (Vertex, VertexID)
+import NN.Vertex.Client (fetchVertex)
 import NN.Vertex.DSL (VertexDSL, VertexDSLF(..))
-import NN.Vertex.Style (Style(..))
 
 interpret
     :: ∀ eff
@@ -39,10 +35,5 @@ runVertexDSL
 runVertexDSL vertexBus = foldFree go
     where
     go :: VertexDSLF ~> Aff (ajax :: AJAX, avar :: AVAR | eff)
-    go (GetVertex (VertexID vertexID) a) = do
-        {response} <- Affjax.get ("/api/v1/vertices?vertexID=" <> vertexID)
-        pure $ a $
-            {note: _, children: _, style: Normal}
-                <$> (response # JSON.toObject >>= StrMap.lookup "note" >>= JSON.toString)
-                <*> (response # JSON.toObject >>= StrMap.lookup "children" >>= JSON.toArray >>= traverse JSON.toString <#> map VertexID <#> List.fromFoldable)
+    go (GetVertex vertexID a) = a <$> fetchVertex vertexID
     go (VertexBus a) = pure $ a vertexBus
---
+++ src/NN/Vertex/Client.purs
@@ -0,0 +1,20 @@
+module NN.Vertex.Client
+( fetchVertex
+) where
+
+import Data.Argonaut.Core as JSON
+import Data.List as List
+import Data.StrMap as StrMap
+import Network.HTTP.Affjax (AJAX)
+import Network.HTTP.Affjax as Affjax
+import NN.Prelude
+import NN.Vertex (Vertex, VertexID(..))
+import NN.Vertex.Style (Style(..))
+
+fetchVertex :: ∀ eff. VertexID -> Aff (ajax :: AJAX | eff) (Maybe Vertex)
+fetchVertex (VertexID vertexID) = do
+    {response} <- Affjax.get ("/api/v1/vertices?vertexID=" <> vertexID)
+    pure $
+        {note: _, children: _, style: Normal}
+            <$> (response # JSON.toObject >>= StrMap.lookup "note" >>= JSON.toString)
+            <*> (response # JSON.toObject >>= StrMap.lookup "children" >>= JSON.toArray >>= traverse JSON.toString <#> map VertexID <#> List.fromFoldable)
